b1c4a4e5a3a003991e1c5c498f497435f7db3aa7
Aryan Ramesh Waghmare <114375352+Aryan270123@users.noreply.github.com>
2024-07-20 16:36:51 +0530

    Add files via Upload

--- Aryan/TNSIF/Training/src/com/DaySix/String1.java
+++ Aryan/TNSIF/Training/src/com/DaySix/String1.java
@@ -5,15 +5,15 @@ public class String1 {
 
 	public static void main(String[] args) {
 //		String  s = new String();
-//		String s = new String("Soham");// imputable object
-//		s.concat(" Niungare");
-//		StringBuffer s2 = new StringBuffer("Soham");
-//		s2.append(" Niungare");
+//		String s = new String("Aryan");// imputable object
+//		s.concat(" Waghmare");
+//		StringBuffer s2 = new StringBuffer("Aryan");
+//		s2.append(" Waghmare");
 //		String s3 = "Hello";
 //		System.out.println(s);
 //		System.out.println(s2);
 //		
-		String s = new String("   Soham   ");
+		String s = new String("   Aryan   ");
 		System.out.println(s.length());
 		System.out.println(s.toLowerCase());
 		String upper = s.toUpperCase();
@@ -23,7 +23,7 @@ public class String1 {
 		System.out.println(s1.equalsIgnoreCase("mumbai"));
 		System.out.println(s1.replace('M','t'));
 		System.out.println(s.trim());
-		String a = "Soham Sanjay Niungare";
+		String a = "Aryan Ramesh Waghmare";
 		System.out.println(a.substring(2));
 		System.out.println(a.substring(2, 6));
 		
