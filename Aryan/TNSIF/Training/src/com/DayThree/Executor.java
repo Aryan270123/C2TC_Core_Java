f8059ac4a9f3125bdde6c1d5834ee40c4f21e28d
Aryan Ramesh Waghmare <114375352+Aryan270123@users.noreply.github.com>
2024-07-20 16:46:31 +0530

    Add files via Upload

--- Aryan/TNSIF/Training/src/com/DayThree/Executor.java
+++ Aryan/TNSIF/Training/src/com/DayThree/Executor.java
@@ -3,9 +3,9 @@ package com.DayThree;
 public class Executor {
 
 	public static void main(String[] args) {
-		Address add1 = new Address("Shivaji Nagar", "ShivSena Shaka", "Mumbai", 400083);
+		Address add1 = new Address("M.G Complex", "Sector-14", "Vashi", 400703);
 		
-		Person p1 = new Person("Soham", add1);
+		Person p1 = new Person("Aryan", add1);
 		
 		
 //		System.out.println(p1);
