be2dffcfe0d4f7c3771f4adfc0ba7d31e47f2f84
Aryan Ramesh Waghmare <114375352+Aryan270123@users.noreply.github.com>
2024-07-20 16:33:54 +0530

    Upload files via update

--- Aryan/TNSIF/Training/src/com/DaySeven/EmployeeExecutor.java
+++ Aryan/TNSIF/Training/src/com/DaySeven/EmployeeExecutor.java
@@ -2,7 +2,7 @@ package com.DaySeven;
 
 public class EmployeeExecutor {
 	public static void main(String[] args) {
-		Employee c1 = new Employee("Soham", 97);
+		Employee c1 = new Employee("Aryan", 97);
 		System.out.println(c1.toString());
 	}
 }
